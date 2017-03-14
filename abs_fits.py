a64d135d3e859f9043a5067c2f954b80d930c5b7
Tom Wood <thomas.wood@stfc.ac.uk>
2017-03-14 14:46:31 +0000

    Ready to churn through data

--- abs_fits.py
+++ abs_fits.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import leastsq
+import scipy.optimize as opt
 import pandas as pd
 from numba import jit
 
@@ -28,14 +28,18 @@ def fit_to_cosh_sinh(y, fy, guesses, fit_array=True):
         fy_calc = a * np.sinh(b * y) + c * np.cosh(d * y) + e
         err = np.abs(fy - fy_calc)
         return err
-    p = list(leastsq(residuals, guesses, args=(y, fy)))[0]
+    p = list(opt.leastsq(residuals, guesses, args=(y, fy)))[0]
     if fit_array:
         a, b, c, d, e = p
         fy_fit = a * np.sinh(b * y_fit) + c * np.cosh(d * y_fit) + e
         return p, y_fit, fy_fit, fy_guess
     return p
 
-########this one doesn't work very well##########
+def fig_setup():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    return fig, ax
+
 @jit(nopython=True)
 def res_hyp(guesses, y, fy):
     a, b, c, d, e = guesses
@@ -44,22 +48,78 @@ def res_hyp(guesses, y, fy):
     err = np.sum(np.abs(fy - fy_calc))
     return err
 
-########this one doesn't work very well either##########
+@jit(nopython=True)
+def eval_re(guesses, y):
+    a, b, c, d, e = guesses
+    fy_calc = (a * np.exp(b * y) + c * np.exp(d * y)) * e * \
+              np.sqrt((1 - y**2))
+    return fy_calc
+
 @jit(nopython=True)
 def res_exp(guesses, y, fy):
-    a, b, c, d, e = guesses
-    fy_calc = (a * np.sinh(b * y) + c * np.cosh(d * y)) * e * \
-              np.sqrt((1 - y**2))
+    fy_calc = eval_re(guesses, y)
     err = np.sum(np.abs(fy - fy_calc))
     return err
 
 @jit(nopython=True)
+def eval_rec(guesses, y):
+    a, b, c, d, e, f = guesses
+    fy_calc = (a * np.exp(b * y) + c * np.exp(d * y) + f) * e * \
+              np.sqrt((1 - y**2))
+    return fy_calc
+
+@jit(nopython=True)
 def res_exp_con(guesses, y, fy):
+    fy_calc = eval_rec(guesses, y)
+    err = np.sum(np.abs(fy - fy_calc))
+    return err
+
+@jit(nopython=True)
+def eval_rel(guesses, y):
+    a, b, c, d, e, f, g = guesses
+    fy_calc = (a * np.exp(b * y) + c * np.exp(d * y) + f + g * y) * e * \
+              np.sqrt((1 - y**2))
+    return fy_calc
+
+@jit(nopython=True)
+def res_exp_lin(guesses, y, fy):
+    fy_calc = eval_rel(guesses, y)
+    err = np.sum(np.abs(fy - fy_calc))
+    return err
+
+@jit(nopython=True)
+def eval_rhc(guesses, y):
     a, b, c, d, e, f = guesses
     fy_calc = (a * np.sinh(b * y) + c * np.cosh(d * y) + f) * e * \
               np.sqrt((1 - y**2))
+    return fy_calc
+
+@jit(nopython=True)
+def res_hyp_con(guesses, y, fy):
+    fy_calc = eval_rhc(guesses, y)
     err = np.sum(np.abs(fy - fy_calc))
     return err
+
+@jit(nopython=True)
+def eval_rhl(guesses, y):
+    a, b, c, d, e, f, g = guesses
+    fy_calc = (a * np.sinh(b * y) + c * np.cosh(d * y) + f + g * y) * e * \
+              np.sqrt((1 - y**2))
+    return fy_calc
+
+@jit(nopython=True)
+def res_hyp_lin(guesses, y, fy):
+    fy_calc = eval_rhl(guesses, y)
+    err = np.sum(np.abs(fy - fy_calc))
+    return err
+
+#try doing differential evolution fits:
+def fit_data(bounds, y, fy, f_eval, f_err, y_fit=None):
+    optres = opt.differential_evolution(f_err, bounds, args=(y, fy))
+    if y_fit is not None:
+        fy_fit = f_eval(optres.x, y_fit)
+        return optres, fy_fit
+    return optres
 
 #Now try putting into polar coordinates ####doesn't help
 def get_polar(y, fy):
@@ -67,3 +127,33 @@ def get_polar(y, fy):
     dists = np.sqrt(y**2 + fy**2)
     angles = np.arctan2(fy, y)
     return dists, angles
+
+y_fit = np.linspace(-1, 1, 1000)
+max_b = 1e8
+bounds_rhc = [(-max_b, max_b), (-10, 10), (-max_b, max_b), (-10, 10),
+              (-max_b, max_b), (-max_b, max_b)]
+bounds_rel = bounds_rhc + [(-max_b, max_b)]
+th_i, mu_i = 0, 50
+
+#fit_attempts = [fit_data(bounds_rhc[:-1], yvals, fy[th_i, mu_i, :], eval_re,
+#                         res_exp, y_fit) for i in range(10)]
+#fit_attempts = [fit_data(bounds_rhc, yvals, fy[th_i, mu_i, :], eval_rec,
+#                         res_exp_con, y_fit) for i in range(10)]
+#fit_attempts = [fit_data(bounds_rel, yvals, fy[th_i, mu_i, :], eval_rel,
+#                         res_exp_lin, y_fit) for i in range(10)]
+#fit_attempts = [fit_data(bounds_rel, yvals, fy[th_i, mu_i, :], eval_rhl,
+#                         res_hyp_lin, y_fit) for i in range(10)]
+#
+#fig, ax = fig_setup()
+#ax.plot(yvals, fy[th_i, mu_i, :], 'o')
+#for i in range(len(fit_attempts)):
+#    ax.plot(y_fit, fit_attempts[i][1])
+
+#fit_attempts1 = [fit_data(bounds_rhc, yvals, fy[th_i, mu_i, :], eval_rhc,
+#                         res_hyp_con, y_fit) for i in range(10)]
+#fig1, ax1, = fig_setup()
+#ax1.plot(yvals, fy[th_i, mu_i, :], 'o')
+#for i in range(len(fit_attempts1)):
+#    ax1.plot(y_fit, fit_attempts1[i][1])
+
+#eval_rhc and res_hyp_con seem to work ok, so now to churn through a muval
